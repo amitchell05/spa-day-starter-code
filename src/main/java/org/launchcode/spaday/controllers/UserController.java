1fc759bdba529228249d4570f4968c06f7f3d98a
Angela Mitchell <anglarenmit@gmail.com>
2022-06-02 00:44:12 -0500

    process form submission for spa app

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,8 +1,11 @@
 package org.launchcode.spaday.controllers;
 
+import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,4 +18,12 @@ public class UserController {
         return "user/add";
     }
 
+    @PostMapping("add")
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        if (verify.equals(user.getPassword())) {
+            return "user/index";
+        }
+
+        return "user/add";
+    }
 }
