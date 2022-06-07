38c757f78689ec41a735afa13c201a0966378b8d
Angela Mitchell <anglarenmit@gmail.com>
2022-06-06 23:15:30 -0500

    create user and validate user creation fields

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,12 +1,27 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
 public class User {
+    private int id;
+    private static int nextId = 1;
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 5, max = 15, message="Username must be between 5 and 15 characters long")
     private String username;
+
+    @Email(regexp = "^[A-Za-z0-9+_.-]+@(.+)$", message = "Please enter a valid email address.")
     private String email;
+
+    @NotBlank(message = "Please enter a password")
     private String password;
 
     public User() {
-
+        id = nextId;
+        nextId++;
     }
 
     public User(String username, String email, String password) {
@@ -14,6 +29,10 @@ public class User {
         this.username = username;
         this.email = email;
         this.password = password;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getUsername() {
@@ -39,4 +58,18 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
+
