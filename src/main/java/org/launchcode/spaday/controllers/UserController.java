9172acf7a12482af8bf7f28a3f59797488e2783a
Angela Mitchell <anglarenmit@gmail.com>
2022-06-13 17:54:19 -0500

    make validation more specific

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -20,7 +20,10 @@ public class UserController {
 
     @PostMapping
     public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Errors errors) {
-        if (errors.hasErrors()) {
+        if (!user.getPassword().equals(user.getVerifyPassword())) {
+            model.addAttribute("error", "Passwords do not match.");
+            return "user/add";
+        } else if (errors.hasErrors()) {
             model.addAttribute("error", "An error occurred.");
             return "user/add";
         } else {
